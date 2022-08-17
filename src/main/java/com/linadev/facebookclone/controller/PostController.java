8c49be14fbf8d7f9dfc13f8df59787908818df19
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-16 21:27:51 -0500

    se crea post en model

--- src/main/java/com/linadev/facebookclone/controller/PostController.java
+++ src/main/java/com/linadev/facebookclone/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.linadev.facebookclone.controller;
 
+import com.linadev.facebookclone.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/post")
 @CrossOrigin(value="http://localhost:3000/")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+
 }
--- src/main/java/com/linadev/facebookclone/service/PostService.java
+++ src/main/java/com/linadev/facebookclone/service/PostService.java
@@ -1,8 +1,13 @@
 package com.linadev.facebookclone.service;
 
 
+import com.linadev.facebookclone.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService implements PostServiceInterface{
+
+    private final PostRepository postRepository;
 }
