444bc9c774cd1e7648a4214ad44a2447592b7f60
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-16 20:35:10 -0500

    se crea PostRepository

--- src/main/java/com/linadev/facebookclone/repository/PostRepository.java
+++ src/main/java/com/linadev/facebookclone/repository/PostRepository.java
@@ -1,2 +1,10 @@
-package com.linadev.facebookclone.repository;public interface PostRepository {
+package com.linadev.facebookclone.repository;
+
+
+import com.linadev.facebookclone.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, String> {
 }
